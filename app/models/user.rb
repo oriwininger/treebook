5475eaf5fe40cc5cd6fdcba67b7de9f23e49a568
Ori Wwininger <oriwininger0@gmail.com>
2013-07-14 16:10:06 +0300

    added gravatars tp profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name, :full_name, :id
    has_many :statuses
+
+   def gravatar_url
+   	stripped_email = email.strip
+   downcased_email = stripped_email.downcase
+   hash = Digest::MD5.hexdigest(downcased_email)
+   "http://gravatar.com/avatar/#{hash}"
+   end
 end
