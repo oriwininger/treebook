9ae35c4020c1cd5ab5b004c2031e446164a47c44
Ori Wwininger <oriwininger0@gmail.com>
2013-07-15 09:04:11 +0300

    unused devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery 
   before_filter :url
   before_filter :authenticate_user!
-  before_filter :redis_counter
+#  before_filter :redis_counter
   
-  def redis_counter
-    if cookies[:couqwvntebbcrq3ouhvddt] != "bvqoroqvoqvoqriqo" or cookies[:couqwvntebbcrq3ouhvddt].present?
-      $redis.set "entrances_counter", 0
-      $redis.incr "entrances_counter"
-      cookies[:couqwvntebbcrq3ouhvddt] = "bvqoroqvoqvoqriqo"
-    end
-  end   
+ # def redis_counter
+  #  if cookies[:couqwvntebbcrq3ouhvddt] != "bvqoroqvoqvoqriqo" or cookies[:couqwvntebbcrq3ouhvddt].present?
+   #   $redis.set "entrances_counter", 0
+    #  $redis.incr "entrances_counter"
+    #  cookies[:couqwvntebbcrq3ouhvddt] = "bvqoroqvoqvoqriqo"
+    #end
+ # end   
   
   def url
   	@current_url = "http://" + "#{request.host}" + "#{request.fullpath}"
